6de57c23785a855c6275605905fb25bfb136635a
hongyi su <mikesu240@gmail.com>
2022-03-28 17:53:49 +0200

    add doc/result

--- map_sta_example.m
+++ map_sta_example.m
@@ -28,11 +28,15 @@ for i=1:length(TT_x)
     hold on
 end
 %obre 69; seal 119; sndb 85; vasq 42; anav 77
-list_5 = [69 119 85 42 77];
+%list_5 = [69 119 85 42 77];
+%fccb 64, sati 15, balp 96, eliz 70, lact 65
+%list_5 = [64 15 96 70 65];
+%lacy 52, pacd 9, plmd 88, uclg 34, vrns 67
+list_5 = [52 9 88 34 67];
 for(i=1:length(list_5))
     plot(TT_x(list_5(i)),TT_y(list_5(i)),'Color','r','DisplayName', 'observational',...
         'Marker','o', 'MarkerSize',dist2fault(list_5(i)));
 end
  text(TT_x(121)*1.004,TT_y(121)*0.999,'71.5km [for reference]');
  title('Location of the 5 selected stations (red circled)','FontSize', 20);
- saveas(fig,[pwd '/example/' 'Selected_location'],'jpg');
+ saveas(fig,[pwd '/example/' 'Selected_location_fccb'],'jpg');
